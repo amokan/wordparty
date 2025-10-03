b89363ccc88f7e7961a3863aea1f5643d26f703f
Adam Mokan <amokan@gmail.com>
2025-10-03 13:19:46 -0700

    fix build error

--- app/auth/sign-up-success/page.tsx
+++ app/auth/sign-up-success/page.tsx
@@ -5,7 +5,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Page() {
